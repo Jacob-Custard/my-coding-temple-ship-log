19de8de54ccfe06c7721be7a79979fb2c0d594c9
Jacob-Custard <jacobbc5064@gmail.com>
2024-10-14 09:48:27 -0500

    M2L1excercise1

---
+++ M2L1exercise1.py
@@ -0,0 +1,15 @@
+#this is a variable for question 1
+a = '''My favorite hobby is playing video games.
+I really enjoy getting to explore different worlds and settings.
+The things people come up with and the ways they're able to bring them to life are fascinating to me.
+Plus who doesnt want to be a badass spacer traveling the cosmos or an awesome merc in Nightcity?'''
+
+#this is my function for question 1
+print(a)
+
+#these are variables for question 2
+a = 5
+c = 8
+
+#this is the function for question 2
+print(a + c)
