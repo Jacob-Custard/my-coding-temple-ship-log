45b2e1eb464fe4fcbed0d14cc7e21b26674e5bb0
Jacob-Custard <jacobbc5064@gmail.com>
2024-10-14 12:33:14 -0500

    M2L1T2 exercises

---
+++ M2L1T2exercise1-9.py
@@ -0,0 +1,34 @@
+#exercise 1
+myFullName = "Jacob Custard"
+
+#exercise 2
+myName = "Jacob"
+myname = "Custard"
+
+#exercise 3
+fav_color = "green"
+print(fav_color)
+
+#exercise 4
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+
+#exercise 5
+Vari1, Vari2, Vari3 = 5, 15.25, "Hello World"
+print(type(Vari1))
+print(type(Vari2))
+print(type(Vari3))
+
+#exercise 6
+mystery_variable = "1234"
+print("The mystery variable is:", type(mystery_variable))
+
+#exercise 7
+dynamicVar = 5
+dynamicVar = str(dynamicVar)
+
+#exercise 8
+if = 15
+print(if) #this returns "SyntaxError: invalid syntax" because "if" is a keyword and can't be used as a variable
+
+#exercise 9
+PI = 3.14159
